1a2b9635b2b2ca4df8ed80affe1fd16cb2bdfc2d
Neil Taitt <ntaitt@gmail.com>
2021-02-14 16:04:44 -0500

    interim changes

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyledGame, StyledScore, StyledTimer, StyledCharacter } from "../styled/Game";
 import { Strong } from "../styled/Random";
 
 export default function Game() {
+    const [score, setScore] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setScore((prevScore) => { return prevScore + 1; });
+        }, 1000);
+        return () => { clearInterval(interval) }
+    }, [score]);
+
+
+
     return (
         <StyledGame>
-            <StyledScore>Score: <Strong>0</Strong></StyledScore>
+            <StyledScore>Score: <Strong>{score}</Strong></StyledScore>
             <StyledCharacter>A</StyledCharacter>
             <StyledTimer>Time: <Strong>00.000</Strong></StyledTimer>
         </StyledGame>
