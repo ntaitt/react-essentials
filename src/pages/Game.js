eef6bdc90d596da7cd29204855372473d4be2612
Neil Taitt <ntaitt@gmail.com>
2021-02-14 17:34:13 -0500

    implemented the timer

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,23 +2,58 @@ import React, { useState, useEffect } from "react";
 import { StyledGame, StyledScore, StyledTimer, StyledCharacter } from "../styled/Game";
 import { Strong } from "../styled/Random";
 
-export default function Game() {
-    const [score, setScore] = useState(0);
+export default function Game({ history }) {
+    const [score, setScore] = useState(1);
+    const MAX_SECONDS = 5;
+    const [ms, setMs] = useState(0);
+    const [seconds, setSeconds] = useState(MAX_SECONDS);
 
+
+    /*previous sample
     useEffect(() => {
         const interval = setInterval(() => {
             setScore((prevScore) => { return prevScore + 1; });
         }, 1000);
         return () => { clearInterval(interval) }
-    }, [score]);
+    }, [score]); */
 
+    useEffect(() => {
+        const currentTime = new Date();
+        const interval = setInterval(() => updateTime(currentTime), 1);
+        return () => clearInterval(interval);
+    }, []);
 
+    const updateTime = (startTime) => {
+        const endTime = new Date();
+        const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+        const formattedMSString = ('0000' + msPassedStr).slice(-5);
+        //00000 -  first 2 are hte seconds, last 3 are the ms that have passed
+        const updatedSeconds = MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
+        const updatedMs = 1000 - parseInt(formattedMSString.substring(formattedMSString.length - 3));
+
+        setSeconds(addLeadingZeros(updatedSeconds, 2));
+        setMs(addLeadingZeros(updatedMs, 3));
+    };
+
+    const addLeadingZeros = (num, length) => {
+        let zeros = '';
+        for (let i = 0; i < length; i++) {
+            zeros += "0";
+        }
+        return (zeros + num).slice(-length);
+    };
+
+    useEffect(() => {
+        if (seconds <= -1) {
+            history.push('/gameOver');
+        }
+    }, [seconds, ms, history]);
 
     return (
         <StyledGame>
             <StyledScore>Score: <Strong>{score}</Strong></StyledScore>
             <StyledCharacter>A</StyledCharacter>
-            <StyledTimer>Time: <Strong>00.000</Strong></StyledTimer>
+            <StyledTimer>Time: <Strong>{seconds}.{ms}</Strong></StyledTimer>
         </StyledGame>
     )
 }
